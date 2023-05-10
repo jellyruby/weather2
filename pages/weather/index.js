f05faa07d9dffd64f5b2ceea5bfedc548997cc44
jellyruby <50604410+jellyruby@users.noreply.github.com>
2023-05-10 21:08:34 +0900

    좌표정보를 가져와서 화면에 출력

--- pages/weather/index.js
+++ pages/weather/index.js
@@ -1,12 +1,44 @@
 import AppLayout from "/components/layout/AppLayout";
 import ContentLayout from "/components/layout/ContentLayout";
+import {useEffect,useState} from 'react';
+
+function getLocation() {
+  
+  
+}
 
 
 const Weather = () => {
+
+  const [location, setLocation] = useState({}); // [변수, 함수
+
+
+  useEffect(() => {
+
+    if (navigator.geolocation) {
+      // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(
+        position => setLocation(position),
+        error => console.error(error),
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: 5000 },
+      );
+    } else {
+      alert('GPS를 지원하지 않습니다. ');
+    }
+  
+
+  }, []);
+
+  console.log(location);
+
+
   return (
     <AppLayout>
       <ContentLayout col={3} row={3}>
-        
+        당신의 위치 lat:{location?.coords?.latitude} lon:{location?.coords?.longitude}
       </ContentLayout>
     </AppLayout>
   );
